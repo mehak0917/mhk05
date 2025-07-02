f654a1202738d8571575d2ae82a55b096785fb50
mehak0917 <mehak5002sharma@gmail.com>
2025-07-02 12:51:56 +0530

    Java

---
+++ JavaTextyr/Firstjava.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Firstjava{
+public static void main(String[] args){
+  System.out.println("Mehak Sharma");
+
+Scanner sc = new Scanner(System.in);
+int n =sc.nextInt();
+
+String name = "Mehak Sharma";
+System.out.println(name);
+
+int rollNo = 1918;
+System.out.println(rollNo);
+ 
+}
+}
